93ec6057facb2b04d6c2d2a528819d50e8e53452
KingJc232 <kingjc232@gmail.com>
2022-03-01 00:07:09 -0800

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+// import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Adding the boot strap css 
+
+// importing the App.css
+import './App.css';
+// importing the boot strap css
 const App = () => {
   return (
     <main>
-      
+        <div>
+          <h1>
+            Hello World
+          </h1>
+        </div>
     </main>
   );
 }
